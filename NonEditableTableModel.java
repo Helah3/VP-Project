e319656b82d5e23ef954dd92e319689fd1a5be8b
Leel15 <150535408+Leel15@users.noreply.github.com>
2024-11-26 21:56:05 +0300

    Update NonEditableTableModel.java

--- NonEditableTableModel.java
+++ NonEditableTableModel.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ProjectPackage;
 
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author helah
- */
+
 public class NonEditableTableModel extends DefaultTableModel {
     public NonEditableTableModel(Object[] columnNames, int rowCount) {
         super(columnNames, rowCount);
